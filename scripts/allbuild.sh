f6b9b3075ca51c7c20285016b3d4e82998c95764
allanin <code@allanin.org>
2017-11-12 15:31:42 +0100

    Initial commit

---
+++ scripts/allbuild.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+#ToDo: Change make.conf for /build
+
+rm -rf /build
+
+mkdir /build
+mkdir /build/dev
+mkdir /build/proc
+mkdir /build/sys
+mkdir /build/tmp
+chmod -R 777 /build/tmp
+chmod +t /build/tmp
+
+emerge baselayout
+emerge -e --deep --with-bdeps=y --newuse @world
+emerge =dev-lang/python-2.7.14
+emerge linux-firmware ntfs3g openssh wireless-tools
+
+emerge retroarch lakka
+
+mkdir -p /build/storage/.cache/services/
+mkdir /build/storage/roms
+chown -R lakka:lakka /build/storage/
+
+chmod 4755 /build/usr/bin/connmanctl
+chmod 4755 /build/usr/bin/systemctl
+chmod 4755 /build/sbin/shutdown
+
+echo 'export PATH=$PATH:/sbin' >> /build/etc/profile
+
+#etc
+cp -a /etc/asound.conf /build/etc/
+cp -a /etc/fstab /build/etc/
+cp -a /etc/sudoers /build/etc/
+cp -a /etc/passwd /build/etc/
+cp -a /etc/shadow /build/etc/
+cp -a /etc/gshadow /build/etc/
+cp -a /etc/group /build/etc/
+cp -a /etc/os-release /build/etc/
+cp -a /etc/locale.gen /build/etc/
+
+#systemd
+cp -a /etc/systemd/timesyncd.conf /etc/systemd/timesyncd.conf
+
+#active services
+#cd /build
+#ln -s etc/systemd/system/multi-user.target.wants/sshd.service usr/lib/systemd/system/sshd.service
+#ln -s etc/systemd/system/multi-user.target.wants/systemd-networkd.service usr/lib/systemd/system/systemd-networkd.service
+#ln -s etc/systemd/system/sockets.target.wants/systemd-networkd.socket usr/lib/systemd/system/systemd-networkd.socket
+#ln -s etc/systemd/system/dbus-fi.w1.wpa_supplicant1.service usr/lib/systemd/system/wpa_supplicant.service
+#ln -s etc/systemd/system/multi-user.target.wants/wpa_supplicant.service usr/lib/systemd/system/wpa_supplicant.service
+#ln -s etc/systemd/system/dbus-org.bluez.service usr/lib/systemd/system/bluetooth.service
+#ln -s etc/systemd/system/bluetooth.target.wants/bluetooth.service usr/lib/systemd/system/bluetooth.service
+
+emerge --unmerge gcc
+emerge --unmerge portage
+emerge --unmerge gcc-config
+emerge --unmerge make
+emerge --unmerge patch
+emerge --unmerge man
+emerge --unmerge man-pages
+emerge --unmerge man-pages-posix
+emerge --unmerge binutils
+emerge --unmerge binutils-config
+emerge --unmerge gnuconfig
---
+++ scripts/allsdk.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+passwd -d root
+
+eselect profile set 9
+
+source /etc/profile
+env-update
+etc-update --automode -3
+locale-gen
+
+emerge dev-vcs/git
+#emerge --sync
+emerge -e --deep --with-bdeps=y --newuse @world
+#emerge --sync
+emerge --depclean
+emerge =dev-lang/python-2.7.14
+emerge linux-firmware ntfs3g wireless-tools
+
+#ToDo for further using nfc
+#emerge libnfc nfc-eventd
+
+emerge retroarch lakka
+
+#ToDo enable when wpa_supplicant does not python 2.7 anymore
+#emerge -C =dev-lang/python-2.7.14
+
+emerge gentoo-sources dracut
+eselect news read
+
+#mv /root/nfceventd.service /etc/systemd/system
+
+mv /root/config /usr/src/linux/.config
+
+#cd /usr/src/linux
+#make -j 4 && make modules_install -j 4 && make install -j 4
+#dracut --force
+
+mkdir -p /storage/.cache/services/
+mkdir /storage/roms
+chown -R lakka:lakka /storage/
+
+
+chmod 4755 /usr/bin/connmanctl
+chmod 4755 /usr/bin/systemctl
+chmod 4755 /sbin/shutdown
+
+echo 'export PATH=$PATH:/sbin' >> /etc/profile
+
+systemctl enable sshd
+systemctl enable systemd-networkd.service
+systemctl enable systemd-resolved.service
+systemctl enable wpa_supplicant.service
+systemctl enable bluetooth.service
+systemctl enable lakka.service
+#systemctl enable nfceventd.service
+systemctl enable connman
+systemctl enable devmon@lakka
+
+hostnamectl set-hostname www.allanin.org
+
+localectl set-locale LANG=de_DE.utf8
+localectl set-keymap de-latin1-nodeadkeys
+
+timedatectl set-local-rtc 0
+timedatectl set-ntp true
+timedatectl set-timezone 'Europe/Berlin'
+
+systemd-machine-id-setup
+
+connmanctl enable ethernet
+connmanctl enable bluetooth
+connmanctl enable wifi
+
+echo ""
+echo "Please change your password with passwd"
+echo ""
+passwd lakka
